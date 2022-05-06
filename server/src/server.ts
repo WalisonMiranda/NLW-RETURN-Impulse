d0d722178566fa3922773d1287568ed4214e5502
Walison Miranda <walisonshido@gmail.com>
2022-05-06 15:17:57 -0300

    .

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,9 +4,7 @@ import { routes } from './routes';
 
 const app = express();
 
-app.use(cors({
-  origin: 'https://nlw-return-impulse-ashy.vercel.app/',
-}));
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
